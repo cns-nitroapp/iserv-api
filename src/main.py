65c69c93cb23b2bcfbfb96f98a3349747ad7b9af
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-09-05 22:38:31 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ headers = {
 
 def load():
     try:
-        querystring = {'target':'/iserv/exercise?filter%5Bstatus%5D=past'}
+        querystring = {'target':'/iserv/exercise?filter%5Bstatus%5D=current'}
         overview = requests.request('POST', host, headers=headers, data=payload , params=querystring)
         soup = BeautifulSoup(overview.text, 'html.parser')
         for link in soup.find_all('a'):
@@ -32,26 +32,37 @@ def extract(url):
     try:
         id = url.replace('https://' + os.getenv('HOST') + '/iserv/exercise/show/', '')
         querystring = {'target':'/iserv/exercise/show/' + id}
-        tasks = requests.request('POST', host, headers=headers, data=payload , params=querystring)
+        if not os.path.exists('../tasks/' + id + '.json'):
+            tasks = requests.request('POST', host, headers=headers, data=payload , params=querystring)
 
-        soup = BeautifulSoup(tasks.text, features='html.parser')
-        description_html = []
-        description = ""
-        div = soup.find_all(class_='text-break-word p-3')
-        for div in soup.find_all(class_='text-break-word p-3'):
-            for p in div.find_all('p'):
-                description_html.append(p.get_text())
-        for line in description_html:
-            description += line
+            soup = BeautifulSoup(tasks.text, features='html.parser')
+            description_html = []
+            description = ""
+            div = soup.find_all(class_='text-break-word p-3')
+            for div in soup.find_all(class_='text-break-word p-3'):
+                for p in div.find_all('p'):
+                    description_html.append(p.get_text())
+            for line in description_html:
+                description += line
 
-        table_of_time = soup.find(class_='bb0').find_all('tr')
-        start = table_of_time[1].find('td').get_text()
-        end = table_of_time[2].find('td').get_text()
+            linkarray = []
 
-        summary = soup.find('h1').get_text()[len('Details for '):]
+            for link in soup.find_all('a'):
+                target = link.get('href')
+                if target.startswith('/iserv/fs/file/'):
+                    data = {"link": 'https://' + os.getenv('HOST') + target}
+                    linkarray.append(data)
 
-        json = {"id": id}, {"title": summary}, {"description": description}, {"start": start}, {"end": end}
-        write(id, json)
+            table_of_time = soup.find(class_='bb0').find_all('tr')
+            start = table_of_time[1].find('td').get_text()
+            end = table_of_time[2].find('td').get_text()
+
+            summary = soup.find('h1').get_text()[len('Details for '):]
+
+            json = {"id": id}, {"title": summary}, {"description": description}, {"start": start}, {"end": end}, {"attachments": linkarray}
+            write(id, json)
+            endpoint(id, summary, description, start, end, linkarray)
+
     except (Exception, ArithmeticError) as e:
             template = Fore.LIGHTRED_EX + 'Error ' + Fore.RESET + '→ An exception of type' + Fore.LIGHTBLUE_EX + ' {0} ' + Fore.RESET + 'occurred.' + Fore.LIGHTBLACK_EX + ' {1!r}'
             message = template.format(type(e).__name__, e.args)
@@ -69,6 +80,47 @@ def write(id, content):
             template = Fore.LIGHTRED_EX + 'Error ' + Fore.RESET + '→ An exception of type' + Fore.LIGHTBLUE_EX + ' {0} ' + Fore.RESET + 'occurred.' + Fore.LIGHTBLACK_EX + ' {1!r}'
             message = template.format(type(e).__name__, e.args)
             print (message)
+
+def endpoint(id, title, description, start, end, linkarray):
+
+    start_date = start.split(' ')
+    start_date = start_date[0]
+    start_date = start_date.split('/')
+    start_date = start_date[2] + '-' + start_date[0] + '-' + start_date[1]
+
+    end_date = end.split(' ')
+    end_date = end_date[0]
+    end_date = end_date.split('/')
+    end_date_year = end_date[2]
+    end_date_month = end_date[0]
+    end_date_day = end_date[1]
+    end_date = end_date[2] + '-' + end_date[0] + '-' + end_date[1]
+
+    print(linkarray)
+
+    if (linkarray == []):
+        data = {
+            'authorization': str('demo'),
+            'id': int(id),
+            'title': title,
+            'description': description + '<br><br>' + ' **Abgabedatum:** ' + end_date_day + '.' + end_date_month + '.' + end_date_year,
+            'start-date': str(start_date),
+            'end-date': str(end_date),
+            'link': 'https://' + os.getenv('HOST') + '/iserv/exercise/show/' + str(id),
+        }
+    else:
+         data = {
+            'authorization': str('demo'),
+            'id': int(id),
+            'title': title,
+            'description': description + '<br><br>' + ' **Abgabedatum:** ' + end_date_day + '.' + end_date_month + '.' + end_date_year,
+            'start-date': str(start_date),
+            'end-date': str(end_date),
+            'link': 'https://' + os.getenv('HOST') + '/iserv/exercise/show/' + str(id),
+            'attachments': linkarray
+        }       
+    requests.request('POST', 'https://api.nitroapp.de/v1/post', headers={'User-Agent': 'IServ', 'Content-Type': 'application/json'}, data=json.dumps(data))
+
         
 
 load()
